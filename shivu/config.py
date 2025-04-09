3d0b14922003fcacef7c068fe33c8bd2aedaa45f
Sunny7849 <164983493+Sunny7849@users.noreply.github.com>
2025-04-09 13:18:19 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,17 +1,17 @@
 lass Config(object):
     LOGGER = True
-    OWNER_ID = "6675050163"
-    sudo_users = ["8043832960","7885908019", "7756901810"]
-    GROUP_ID = -100237291402
-    TOKEN = "7689391231:AAEHXLz_uEv4Kpd-pZHQqVjkPujA7-ObJZw"
-    mongo_url = "mongodb+srv://Hinatalover7:HSCREW7@hinatalover7.rql67.mongodb.net/?retryWrites=true&w=majority&appName=Hinatalover7"  # [>
+    OWNER_ID = "8156600797"
+    sudo_users = ["8156600797","7640076990", "7756901810"]
+    GROUP_ID = -1002343417591
+    TOKEN = "7539465396:AAFT5I6oK0wRJHSFNaAUMosQ4uFm2pHa7_c"
+    mongo_url = "mongodb+srv://arzuquine:TGDARK11798@madarauzumaki.22wjzeb.mongodb.net/?retryWrites=true&w=majority&appName=Madarauzumaki"  # [>
     PHOTO_URL = ["http://postimg.cc/cv6G20zT"]
     SUPPORT_CHAT = "https://t.me/Anime_Circle_Club"
     UPDATE_CHAT = "https://t.me/Waifu_Chan_Bot_updates"
-    BOT_USERNAME = "@Waifu_ChanBbot"
-    CHARA_CHANNEL_ID = "-1002640379822"
-    api_id = 20952516
-    api_hash = "4b7997216605cd4dd9cb2ee513838955"
+    BOT_USERNAME = "Daddy_Madara_WaifuBot"
+    CHARA_CHANNEL_ID = "-1002510711682"
+    api_id = 28159105
+    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
 class Production(Config):  # ✅  Config se inherit ho raha hai
     LOGGER = True
 class Development(Config):  # ✅  Config se inherit ho raha hai
