6a0bcc6b37b18ad114c8ec8d5ddf185dcadf74e0
Sunny7849 <164983493+Sunny7849@users.noreply.github.com>
2025-04-11 02:14:42 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -1,3 +1,26 @@
+import asyncio
+import random
+from html import escape 
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext, CommandHandler
+
+from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, BOT_USERNAME, db, GROUP_ID
+from shivu import pm_users as collection 
+
+# Alag lists for gifs
+PHOTO_URL_PM = [
+    "https://media0.giphy.com/media/BfevCgt1YxDTW/giphy.gif",
+    "https://media4.giphy.com/media/6sv3Z8wXzyEzC/giphy.gif",
+    "https://media4.giphy.com/media/5D8fDjKyQfuZW/giphy.gif"
+]
+
+PHOTO_URL_GC = [
+    "https://media2.giphy.com/media/HXN6ZE2FbnH44/giphy.gif",
+    "https://media1.giphy.com/media/bJ0TSiVhirmlG/giphy.gif",
+    "https://media0.giphy.com/media/8SEnoMhrEeBDa/giphy.gif"
+]
+
 async def start(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
     first_name = update.effective_user.first_name
@@ -9,21 +32,17 @@ async def start(update: Update, context: CallbackContext) -> None:
         return
 
     user_data = await collection.find_one({"_id": user_id})
+
     if user_data is None:
         await collection.insert_one({"_id": user_id, "first_name": first_name, "username": username})
-        await context.bot.send_message(
-            chat_id=GROUP_ID, 
-            text=f"New user Started The Bot..\nUser: <a href='tg://user?id={user_id}'>{escape(first_name)}</a>", 
-            parse_mode='HTML'
-        )
+        await context.bot.send_message(chat_id=GROUP_ID, 
+                                       text=f"New user Started The Bot..\nUser: <a href='tg://user?id={user_id}'>{escape(first_name)}</a>", 
+                                       parse_mode='HTML')
     else:
         if user_data['first_name'] != first_name or user_data['username'] != username:
-            await collection.update_one(
-                {"_id": user_id}, 
-                {"$set": {"first_name": first_name, "username": username}}
-            )
+            await collection.update_one({"_id": user_id}, {"$set": {"first_name": first_name, "username": username}})
 
-    # Show loading animation lines
+    # Summoning animation lines
     loading_lines = [
         "⏳ Initializing summoning core...",
         "⚡ Channeling chakra streams...",
@@ -34,14 +53,12 @@ async def start(update: Update, context: CallbackContext) -> None:
 
     for line in loading_lines:
         await context.bot.send_message(chat_id=update.effective_chat.id, text=line)
-        await asyncio.sleep(1.2)  # slight delay for effect
+        await asyncio.sleep(1.2)
 
     keyboard = [
         [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-        [
-            InlineKeyboardButton("SUPPORT", url='https://t.me/Anime_Circle_Club'),
-            InlineKeyboardButton("UPDATES", url='https://t.me/+vDcCB_w1fxw1YTll')
-        ],
+        [InlineKeyboardButton("SUPPORT", url='https://t.me/Anime_Circle_Club'),
+         InlineKeyboardButton("UPDATES", url='https://t.me/+vDcCB_w1fxw1YTll')],
         [InlineKeyboardButton("HELP", url=f"http://t.me/{BOT_USERNAME}?start=help")],
         [InlineKeyboardButton("SOURCE", url='https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
     ]
@@ -76,10 +93,7 @@ async def start(update: Update, context: CallbackContext) -> None:
         caption = "🎴Alive!?... \nConnect to me in PM for more information"
         gif_url = random.choice(PHOTO_URL_GC)
 
-    await context.bot.send_animation(
-        chat_id=update.effective_chat.id,
-        animation=gif_url,
-        caption=caption,
-        reply_markup=reply_markup,
-        parse_mode='markdown'
-    )
+    await context.bot.send_animation(chat_id=update.effective_chat.id, animation=gif_url, caption=caption, reply_markup=reply_markup, parse_mode='markdown')
+
+# Handler registration
+application.add_handler(CommandHandler('start', start, block=False))
